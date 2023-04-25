02631b0bc1e2562654f6c317130e6bde500d291e
mcanela2900 <130692334+mcanela2900@users.noreply.github.com>
2023-04-25 18:07:03 -0400

    items added

--- rpg system.cpp
+++ rpg system.cpp
@@ -16,7 +16,7 @@ int enemy_speed[2] = {5, 5};
 int enemy_health[2] = {30, 30};
 
 //aditional initializations as to not repeat them in the loop
-bool gamestatehealth = true, pturn, eturn, has_defended;
+bool gamestatehealth = true, pturn, eturn, has_defended, has_speed = true, has_axe = true, has_potion = true;
 
 // this will be used later to determine wether the game is over or not
 int gamestate() {
@@ -68,6 +68,51 @@ int eattack()
     return dmg;
 }
 
+//items
+
+int speedp = 0, axe = 0, potion = 0;
+
+void speed_potion()
+{
+    speedp += 1;
+    if (speedp == 2) {
+        has_speed = false;
+        cout << "You already drank the only speed potion you had!\n";
+    }
+    players_speed[0] += 2;
+    cout << "You feel energized by the effect of the speed potion \n +2 speed \n";
+    
+}
+
+void battle_axe()
+{
+    axe += 1;
+    if (axe == 2) {
+        has_axe = false;
+        cout << "You're already holding your axe!\n";
+    }
+    else {
+    players_attack[0-1] += 3;
+    cout << "You draw your battle axe from your bag and feel like youll just hit the enemy a little harder\n +3 attack \n";
+    }
+}
+
+void health_potion()
+{
+    potion += 1;
+    if (potion == 3){
+        cout << "You search your bag for another potion, but you seem to have run out of them\n";
+        has_potion = false;
+    }
+    else {
+        players_health[1] = players_health[1] + players_health[0] / 2;
+        cout << "You drink the potion and feel like you can fight a little longer \n +10 health\n";
+        if (players_health[1] > players_health[0]) {
+            players_health[1] = players_health[0];
+        }
+    }
+}
+
 void wrong_input() {
     //function to allow for additional inputs
     cout << "please try your input again\n";
@@ -89,19 +134,24 @@ int order() {
 int main() //first loop to keep the game going as long as at least one of them hits 0hp
 {
     srand(time(NULL));
+    cout << "You've encountered an enemy!\n";
     do {
-        string input;
+        string input, ch;
         int turnorder = order();
         if (turnorder == 1) {
+            cout << "Your turn!\n";
             pturn = true;
             eturn = false;
         }
         else
         {
+            cout << "Enemy's turn!\n";
             pturn = false;
             eturn = true;
         }
         while (pturn == true) {
+            cout << "You have the following actions: \n";
+            
             cin >> input;
             switch (input[0]) {
                 case 'a': {
@@ -121,7 +171,7 @@ int main() //first loop to keep the game going as long as at least one of them h
                     pturn = false;
                     break;
                 }
-                 case 'd': {
+                case 'd': {
                     if (has_defended) {
                         cout << "You have already defended this turn.\n";
                         pturn = true;
@@ -132,6 +182,51 @@ int main() //first loop to keep the game going as long as at least one of them h
                     has_defended = true;
                     pturn = false;
                     break;
+                }
+                case 'i': {
+                    cout << "You open your bag and find the following items:\n";
+                    cout << "Speed potion: Increase your speed by 2\n";
+                    cout << "Battle axe: Increase your attack by 3\n";
+                    cout << "Health potion: Increase your health by 50%, max 2 uses\n";
+                    cout << "type the name of the item you wish to use. type 'c' to return\n";
+                    cin >> ch;
+                    switch (ch[0]) {
+                        case 's': {
+                            if (has_speed ==false) {
+                                cout << "You already did this action!\n";
+                            }
+                            else {
+                                speed_potion();
+                                pturn = false;
+                            }
+                            break;
+                        }
+                        case 'b': {
+                            if (has_axe == false) {
+                                cout << "You already did this action!\n";
+                            }
+                            else {
+                                battle_axe();
+                                pturn = false;
+                            }
+                            break;
+                        }
+                        case 'h': {
+                            if (has_potion == false) {
+                                cout << "You already did this action!\n";
+                            }
+                            else {
+                                health_potion();
+                                pturn = false;
+                            }
+                            break;
+                        }
+                        case 'c': {
+                            cout << "You decide to leave them for later and face the enemy again\n";
+                            break;
+                        }
+                    }
+                break;
                 }
                 default: {
                     pturn = true;
